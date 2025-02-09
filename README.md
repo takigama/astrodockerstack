42e18e704ced04e36df16dae28ef06e8fbb915cd
Paul J. Robinson <paul@ictnetworks.com.au>
2025-02-09 15:05:19 +1100

    add indiserver

---
+++ README.md
@@ -0,0 +1,6 @@
+# Docker set for astrophotography
+
+Its a bunch of custom containers that get built for doing astrophotography in linux. Yeah, i love containers a little too much, currently has
+ - Astrometry
+ - gphoto2
+ - indiserver
--- buildit.sh
+++ buildit.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+TO_BUILD="Astrometry gphoto2 indiserver"
+BUILD_TAG="0.1"
 
-docker build --progress=plain -t astrometry:0.1 . > build.log 2>&1
+(
+for i in $TO_BUILD
+do
+	LOWER_NAME=`echo "$i" | tr '[:upper:]' '[:lower:]'`
+	echo; echo; echo
+	echo "Building $i as $LOWER_NAME:$BUILD_TAG"
+	docker build --progress=plain -t $LOWER_NAME:$BUILD_TAG . 2>&1
+done
+) | tee build.log
